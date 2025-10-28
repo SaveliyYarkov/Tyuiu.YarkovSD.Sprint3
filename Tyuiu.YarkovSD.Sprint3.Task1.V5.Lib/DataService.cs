c3520d685dfb5386b08c331b41e1469d35c87f5d
BorneO <savelis1908@gmail.com>
2025-10-28 14:27:08 +0300

    Task1

---
+++ Tyuiu.YarkovSD.Sprint3.Task1.V5.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.YarkovSD.Sprint3.Task1.V5.Lib
+{
+    public class DataService : ISprint3Task1V5
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double a = 0;
+            while (startValue <= 10)
+            {
+                a += Math.Cos(startValue) + 9;
+                startValue++;
+            }
+
+            return Math.Round(a, 3);
+        }
+    }
+}
+}
---
+++ Tyuiu.YarkovSD.Sprint3.Task1.V5/Program.cs
@@ -0,0 +1,6 @@
+﻿using Tyuiu.YarkovSD.Sprint3.Task1.V5.Lib;
+
+DataService ds = new DataService();
+int a = (int)Convert.ToInt32(Console.ReadLine());
+int b = (int)Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ds.GetMultiplySeries(a, b));
