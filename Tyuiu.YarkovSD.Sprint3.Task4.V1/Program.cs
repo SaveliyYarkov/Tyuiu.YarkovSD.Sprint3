d70d9bf5a34e4bcdc73fefe18953e7b6ca04b36a
BorneO <savelis1908@gmail.com>
2025-10-30 11:41:30 +0300

    Task4

--- Tyuiu.YarkovSD.Sprint3.Task4.V1/Program.cs
+++ Tyuiu.YarkovSD.Sprint3.Task4.V1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.YarkovSD.Sprint3.Task4.V1.Lib;
+
+DataService ds = new DataService();
+int a = Convert.ToInt32(Console.ReadLine());
+int s = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ds.Calculate(a, s));
+Console.ReadKey();
